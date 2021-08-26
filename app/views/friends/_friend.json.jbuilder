561cb75851604a6d40e8f2c3f6b1d34009feccb9
manhdang <manhddse03539@gmail.com>
2021-08-26 21:28:34 +0700

    Devise view

---
+++ app/views/friends/_friend.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! friend, :id, :first_name, :lastname, :email, :string, :twister, :phone, :created_at, :updated_at
+json.url friend_url(friend, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  resources :friends
   get 'home/about'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

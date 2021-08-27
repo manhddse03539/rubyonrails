1723dfef39f47612015d3d9bf912f47613566953
manhdang <manhddse03539@gmail.com>
2021-08-28 02:02:46 +0700

    style midifications

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   resources :friends
   get 'home/about'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
